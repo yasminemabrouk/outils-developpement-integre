03604c0785852dc84477c34a9e0d22be56a68b92
yasmine mabrouk <yasmine.mabrouk@he-arc.ch>
2015-10-22 08:40:02 +0200

    initialcommit

---
+++ src/ch/hearc/ig/odi/customeraccont/business/Account.java
@@ -0,0 +1,106 @@
+package ch.hearc.ig.odi.customeraccont.business;
+
+public class Account {
+
+    private String number;
+    private String name;
+    private double balance = 0;
+    private double rate = 0.001;
+    private Customer customer;
+
+    /**
+     *
+     * @param number
+     * @param name
+     * @param rate
+     * @param customer
+     */
+    public Account(String number, String name,  double rate ,Customer customer ) {
+        this.number = number;
+        this.name = name;
+        this.rate = rate;
+        this.customer = customer ;
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void credit(double amount) {
+        balance = balance + amount;
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void debit(double amount) {
+        if (balance < amount) {
+            throw new IllegalStateException() ;
+        }
+        balance = balance - amount;
+    }
+
+    /**
+     *
+     * @param amount
+     * @param source
+     * @param target
+     */
+    public static void transfer(double amount, Account source, Account target) {
+        if (source.getBalance() < amount) {
+            throw new IllegalStateException() ;
+        }
+        source.debit(amount);
+        target.credit(amount);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    /**
+     *
+     * @param balance
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    /**
+     *
+     * @param rate
+     */
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    /**
+     *
+     * @param number
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
---
+++ src/ch/hearc/ig/odi/customeraccont/business/Customer.java
@@ -0,0 +1,87 @@
+package ch.hearc.ig.odi.customeraccont.business;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private int number;
+    private String firstName;
+    private String lastName;
+    private ArrayList<Account> accounts = new ArrayList <Account>() ;
+
+    /**
+     *
+     * @param number
+     * @param firstName
+     * @param lastName
+     */
+    public Customer(int number, String firstName, String lastName) {
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     *
+     * @param number
+     */
+    public Account getAccountByNumber(String number) {
+        int i = 0;
+        Account account = null ;
+        while(i < accounts.size()-1 &&  account == null ) {
+            if (accounts.get(i).getNumber() == number) {
+                account = accounts.get(i);
+            }
+            i++;
+        }
+        return account ;
+    }
+
+    /**
+     *
+     * @param number
+     * @param name
+     * @param rate
+     */
+    public void addAcount(String number, String name, double rate) {
+        Account account = new Account(number , name , rate, this);
+        accounts.add(account);
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     *
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     *
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    /**
+     *
+     * @param number
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
