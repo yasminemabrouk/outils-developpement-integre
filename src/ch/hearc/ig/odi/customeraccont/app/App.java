50af898b194d78ef768efcef4969672c2953eb14
Maël Blaser <mael.blaser@he-arc.ch>
2015-10-22 08:56:55 +0200

    Added app class

---
+++ src/ch/hearc/ig/odi/customeraccont/app/App.java
@@ -0,0 +1,39 @@
+package ch.hearc.ig.odi.customeraccont.app;
+
+import ch.hearc.ig.odi.customeraccont.business.Account;
+import ch.hearc.ig.odi.customeraccont.business.Customer;
+
+public class App {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new App().run();
+    }
+
+    public void run() {
+        Customer customer1 = new Customer(1, "Maël", "Blaser");
+        Customer customer2 = new Customer(2, "Jean", "Pierre");
+        customer1.addAcount("1", "Courant", 0.001);
+        System.out.println("Customer 1, compte 1, balance : " + customer1.getAccountByNumber("1").getBalance());
+        System.out.println("**Customer 1, compte 1, credit 25");
+        customer1.getAccountByNumber("1").credit(25);
+        System.out.println("**Customer 1, compte 1, debit 10");
+        customer1.getAccountByNumber("1").debit(10);
+        System.out.println("Customer 1, compte 1, balance : " + customer1.getAccountByNumber("1").getBalance());
+        customer2.addAcount("1", "Courant", 0.001);
+        System.out.println("**Customer 1, compte 1, transfert of 10 to Customer 2, compte 1");
+        customer1.getAccountByNumber("1").transfer(10, customer1.getAccountByNumber("1"), customer2.getAccountByNumber("1"));
+        System.out.println("Customer 1, compte 1, balance : " + customer1.getAccountByNumber("1").getBalance());
+        System.out.println("Customer 2, compte 1, balance : " + customer2.getAccountByNumber("1").getBalance());
+        customer1.addAcount("2", "Epagrne", 0.002);
+        System.out.println("Customer 1, compte 2, balance : " + customer1.getAccountByNumber("2").getBalance());
+        System.out.println("**Customer 1, compte 1, transfert of 5 to Customer 1, compte 2");
+        customer1.getAccountByNumber("1").transfer(5, customer1.getAccountByNumber("1"), customer1.getAccountByNumber("2"));
+        System.out.println("Customer 1, compte 1, balance : " + customer1.getAccountByNumber("1").getBalance());
+        System.out.println("Customer 1, compte 2, balance : " + customer1.getAccountByNumber("2").getBalance());
+    }
+
+}
